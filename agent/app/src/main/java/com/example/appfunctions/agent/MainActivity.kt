d97595f4777361dcf0f1c30b9ef6d772d07452e8
skateryna <k.semyonova@gmail.com>
2026-07-27 19:59:27 -0700

    spotlessApply

--- agent/app/src/main/java/com/example/appfunctions/agent/MainActivity.kt
+++ agent/app/src/main/java/com/example/appfunctions/agent/MainActivity.kt
@@ -66,7 +66,6 @@ import dagger.hilt.android.AndroidEntryPoint
  */
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
